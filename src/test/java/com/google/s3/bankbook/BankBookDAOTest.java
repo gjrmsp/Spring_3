16179e509f4d6fb6ca0937288a0138c04a64f75b
gjrmsp <gjrmsp@naver.com>
2021-03-23 14:26:25 +0900

    Update BankBookDAOTest.java

--- src/test/java/com/google/s3/bankbook/BankBookDAOTest.java
+++ src/test/java/com/google/s3/bankbook/BankBookDAOTest.java
@@ -2,6 +2,8 @@ package com.google.s3.bankbook;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,6 +15,40 @@ public class BankBookDAOTest extends MyAbstractTest {
 	private BankBookDAO bankBookDAO;
 	
 	@Test
+	public void setUpdateTest()throws Exception{
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		bankBookDTO.setBooknumber(5);
+		
+		bankBookDTO = bankBookDAO.getSelect(bankBookDTO);
+		
+		bankBookDTO.setBookname("New Poduct");
+		
+		int result = bankBookDAO.setUpdate(bankBookDTO);
+		assertEquals(1, result);
+	}
+	
+	//@Test
+	public void setDeleteTest()throws Exception{
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		bankBookDTO.setBooknumber(9);
+		int result = bankBookDAO.setDelete(bankBookDTO);
+		
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void getListTest()throws Exception{
+		List<BankBookDTO> ar = bankBookDAO.getList();
+		assertNotEquals(0, ar.size());
+	}
+	
+	//@Test
+	public void getSelectTest()throws Exception{
+		BankBookDTO bankBookDTO = bankBookDAO.getSelect(null);
+		assertNotNull(bankBookDTO);
+	}
+	
+	//@Test
 	public void setWriteTest() throws Exception {
 		BankBookDTO bankBookDTO = new BankBookDTO();
 		bankBookDTO.setBookname("Test");
@@ -23,4 +59,6 @@ public class BankBookDAOTest extends MyAbstractTest {
 		assertEquals(1, result);
 	}
 	
+	
+
 }
