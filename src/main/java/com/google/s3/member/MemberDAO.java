22696750f0ef371aa3e5118981bdbe19bc638157
gjrmsp <gjrmsp@naver.com>
2021-03-23 16:03:58 +0900

    update

--- src/main/java/com/google/s3/member/MemberDAO.java
+++ src/main/java/com/google/s3/member/MemberDAO.java
@@ -18,68 +18,27 @@ public class MemberDAO {
 	private SqlSession sqlsession;
 	private final String NAMESPACE="com.google.s3.member.MemberDAO.";
 	
-	public int setUpdate(MemberDTO memberDTO) throws Exception {
-		return sqlsession.update(NAMESPACE+"setUpdate", memberDTO);
-	}
+//	//Update
+//	public int memberUpdate(MemberDTO memberDTO) throws Exception {	
+//		//id를 제외하고 나머지 수정
+//		return 
+//	}
+//	
+//	//Delete
+//	public int memberDelete(MemberDTO memberDTO) throws Exception{	
+//		
+//		return 
+//	}
+//	
+//	//Join
+//	public int memberJoin(MemberDTO memberDTO) throws Exception{
+//	
+//	}
 
-	public int setDelete(MemberDTO memberDTO) throws Exception {
-		return sqlsession.delete(NAMESPACE+"setDelete", memberDTO);
-	}
-
-	public int setWrite(MemberDTO memberDTO) throws Exception {
-		return sqlsession.insert(NAMESPACE+"setWrite", memberDTO);
-	}
-
-	//memberJoin 데이터를 받아서 DB에 insert하는 메서드
-	public int memberJoin(MemberDTO memberDTO) throws Exception{
-	
-		
-		return result;
-		
-	}
-
-		//login - id pw를 받아서 조회
+	//login - id pw를 받아서 조회
 	public MemberDTO login(MemberDTO memberDTO) throws Exception{
 
-		//1. 로그인 정보 
-		String user="user01";
-		String password="user01";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
-
-		//2. 클래스 로딩
-		Class.forName(driver);
-
-		//3. 로그인 Connection
-		Connection con = DriverManager.getConnection(url, user, password);
-
-		//4. SQL문 생성
-		String sql ="SELECT * FROM member WHERE id=? and pw=?";
-
-		//5. 미리 보내기
-		PreparedStatement st = con.prepareStatement(sql);
-
-		//6. ? 세팅
-		st.setString(1, memberDTO.getId());
-		st.setString(2, memberDTO.getPw());
-
-		//7. 최종 전송 후 처리
-		ResultSet rs = st.executeQuery();
-
-		if(rs.next()) {
-			memberDTO.setName(rs.getString("name"));
-			memberDTO.setEmail(rs.getString("email"));
-			memberDTO.setPhone(rs.getString("phone"));
-		} else {
-			memberDTO = null;
-		}
-
-		//8. 해제
-		rs.close();
-		st.close();
-		con.close();
-
-		return memberDTO;
+		return sqlsession.selectOne(NAMESPACE+"memberLogin", memberDTO);
 	}
 
 }
