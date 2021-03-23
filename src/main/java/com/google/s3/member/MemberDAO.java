e746af8424af1320c24f6fcaa7be22dbc55d7733
gjrmsp <gjrmsp@naver.com>
2021-03-23 16:09:12 +0900

    Update MemberDAO.java

--- src/main/java/com/google/s3/member/MemberDAO.java
+++ src/main/java/com/google/s3/member/MemberDAO.java
@@ -18,26 +18,25 @@ public class MemberDAO {
 	private SqlSession sqlsession;
 	private final String NAMESPACE="com.google.s3.member.MemberDAO.";
 	
-//	//Update
-//	public int memberUpdate(MemberDTO memberDTO) throws Exception {	
-//		//id를 제외하고 나머지 수정
-//		return 
-//	}
-//	
-//	//Delete
-//	public int memberDelete(MemberDTO memberDTO) throws Exception{	
-//		
-//		return 
-//	}
-//	
-//	//Join
-//	public int memberJoin(MemberDTO memberDTO) throws Exception{
-//	
-//	}
+	//Update
+	public int memberUpdate(MemberDTO memberDTO) throws Exception {	
+		//id를 제외하고 나머지 수정
+		return sqlsession.update(NAMESPACE+"memberUpdate",memberDTO);
+	}
+	
+	//Delete
+	public int memberDelete(MemberDTO memberDTO) throws Exception {	
+		return sqlsession.delete(NAMESPACE+"memberDelete",memberDTO);
+	}
+	
+	//Join
+	public int memberJoin(MemberDTO memberDTO) throws Exception {	
+		//id를 제외하고 나머지 수정
+		return sqlsession.insert(NAMESPACE+"memberJoin",memberDTO);
+	}
 
 	//login - id pw를 받아서 조회
 	public MemberDTO login(MemberDTO memberDTO) throws Exception{
-
 		return sqlsession.selectOne(NAMESPACE+"memberLogin", memberDTO);
 	}
 
