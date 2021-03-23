0b2386d3c1c631f1756d29dc5cd189ff26103c3c
gjrmsp <gjrmsp@naver.com>
2021-03-23 14:24:05 +0900

    update

--- src/main/java/com/google/s3/bankbook/BankBookDAO.java
+++ src/main/java/com/google/s3/bankbook/BankBookDAO.java
@@ -7,114 +7,46 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BankBookDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.iu.s3.bankbook.BankBookDAO.";
 
-	public int setWrite(BankBookDTO bankBookDTO) throws Exception{
-		//1. 로그인 정보 
-		String user="user01";
-		String password="user01";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
-
-		//2. 클래스 로딩
-		Class.forName(driver);
-
-		//3. 로그인 Connection
-		Connection con = DriverManager.getConnection(url, user, password);
-
-		String sql ="insert into bankbook values(bank_seq.nextval,?,?,?)";
-
-		PreparedStatement st = con.prepareStatement(sql);
-
-		st.setString(1, bankBookDTO.getBookname());
-		st.setDouble(2, bankBookDTO.getBookrate());
-		st.setString(3, bankBookDTO.getBooksale());
-
-		int result = st.executeUpdate();
-
-		st.close();
-		con.close();
-
+	
+	public int setUpdate(BankBookDTO bankBookDTO)throws Exception{
+		return sqlSession.update(NAMESPACE+"setUpdate", bankBookDTO);
+	}
+	
+	public int setDelete(BankBookDTO bankBookDTO)throws Exception{
+		return sqlSession.delete(NAMESPACE+"setDelete", bankBookDTO);
+	}
+	
+	public int setWrite(BankBookDTO bankBookDTO)throws Exception{
+		int result = sqlSession.insert(NAMESPACE+"setWrite", bankBookDTO);
 		return result;
+
 	}
 
-	public BankBookDTO getSelect(BankBookDTO bankBookDTO) throws Exception{
-		//1. 로그인 정보 
-		String user="user01";
-		String password="user01";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
 
-		//2. 클래스 로딩
-		Class.forName(driver);
-
-		//3. 로그인 Connection
-		Connection con = DriverManager.getConnection(url, user, password);
-
-		String sql =" select * from bankbook where booknumber = ?";
-
-		PreparedStatement st = con.prepareStatement(sql);
-
-		st.setLong(1, bankBookDTO.getBooknumber());
-
-		ResultSet rs = st.executeQuery();
-
-		if(rs.next()) {
-			//bankBookDTO = new BankBookDTO();
-			bankBookDTO.setBooknumber(rs.getLong("bookNumber"));
-			bankBookDTO.setBookname(rs.getString("bookName"));
-			bankBookDTO.setBookrate(rs.getDouble("bookRate"));
-			bankBookDTO.setBooksale(rs.getString("bookSale"));
-
-		}
-
-		rs.close();
-		st.close();
-		con.close();
-
+	public BankBookDTO getSelect(BankBookDTO bankBookDTO)throws Exception{
+			//long num=1L;
+			bankBookDTO = sqlSession.selectOne(NAMESPACE+"getSelect", bankBookDTO);
+		
 		return bankBookDTO;
+
 	}	
+
 
 	//getList
 	//bankbook table의 모든 데이트 조회 후 리턴
-	public List<BankBookDTO> getList() throws Exception{
-		ArrayList<BankBookDTO> ar = new ArrayList<BankBookDTO>();
-
-		//1. 로그인 정보 
-		String user="user01";
-		String password="user01";
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-		String driver = "oracle.jdbc.driver.OracleDriver";
-
-		//2. 클래스 로딩
-		Class.forName(driver);
-
-		//3. 로그인 Connection
-		Connection con = DriverManager.getConnection(url, user, password);
-
-		String sql ="select * from bankbook";
-
-		PreparedStatement st = con.prepareStatement(sql);
-
-		ResultSet rs = st.executeQuery();
-		System.out.println("executeQuery----------");
-		while(rs.next()) {
-			System.out.println("count");
-			BankBookDTO bankBookDTO = new BankBookDTO();
-			bankBookDTO.setBooknumber(rs.getLong("bookNumber"));
-			bankBookDTO.setBookname(rs.getString("bookName"));
-			bankBookDTO.setBookrate(rs.getDouble("bookRate"));
-			bankBookDTO.setBooksale(rs.getString("bookSale"));
-			ar.add(bankBookDTO);
-		}
-
-		rs.close();
-		st.close();
-		con.close();
-
-		return ar;
+	public List<BankBookDTO> getList()throws Exception{
+		return sqlSession.selectList(NAMESPACE+"getList");
 	}
+
 }
---
+++ src/test/java/com/google/s3/bankbook/BankBookDAOTest.java
@@ -0,0 +1,26 @@
+package com.google.s3.bankbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.s3.MyAbstractTest;
+
+public class BankBookDAOTest extends MyAbstractTest {
+
+	@Autowired
+	private BankBookDAO bankBookDAO;
+	
+	@Test
+	public void setWriteTest() throws Exception {
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		bankBookDTO.setBookname("Test");
+		bankBookDTO.setBookrate(0.12);
+		bankBookDTO.setBooksale("Y");
+		int result = bankBookDAO.setWrite(bankBookDTO);
+		
+		assertEquals(1, result);
+	}
+	
+}
