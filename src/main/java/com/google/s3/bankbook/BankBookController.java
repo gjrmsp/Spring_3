be0fdd0f4f73b2ee542e705b348520ece992c7fa
gjrmsp <gjrmsp@naver.com>
2021-03-24 10:40:46 +0900

    update

---
+++ src/main/java/com/google/s3/bankbook/BankBookController.java
@@ -0,0 +1,33 @@
+package com.google.s3.bankbook;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/bankbook/**")
+public class BankBookController {
+
+	@Autowired
+	private BankBookService bankBookService;
+
+	@RequestMapping("bankbookList")
+	public void getList(Model model) throws Exception {
+		List<BankBookDTO> ar = bankBookService.getList();
+		model.addAttribute("list", ar);
+	}
+
+	@RequestMapping(value = "bankbookSelect")
+	public ModelAndView getSelect(BankBookDTO bankBookDTO) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		bankBookDTO = bankBookService.getSelect(bankBookDTO);
+		
+		mv.addObject("dto", bankBookDTO);
+		mv.setViewName("bankbook/banbookSelect");
+		return mv;
+	}
+}
